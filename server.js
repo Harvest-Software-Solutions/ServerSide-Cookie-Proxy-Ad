9a005231a16cb3d0da618432490e383e2623207a
Iam@Tejovardhan.CV <57311212+Iam-tejovardhan@users.noreply.github.com>
2022-02-21 14:12:18 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 /* eslint-disable require-jsdoc */
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 80;
 const request = require('request');
 const cors = require('cors');
 let _chanceBidUrl = '';
