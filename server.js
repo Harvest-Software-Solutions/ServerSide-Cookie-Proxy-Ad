a842a305d77da1c98ec645294d91aa646c1b9bdd
Iam@Tejovardhan.CV <57311212+Iam-tejovardhan@users.noreply.github.com>
2022-02-22 16:40:22 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,47 +2,47 @@
 /* eslint-disable require-jsdoc */
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 3000;
 const request = require('request');
 const cors = require('cors');
-var timeout = require('connect-timeout');
+const timeout = require('connect-timeout');
 
 let _chanceBidUrl = '';
 
 const videoJson1 = [{
-    'src': 'https://d8c1da9d356baba4ecb40074dc4758c5.egress.mediapackage-vod.us-east-1.amazonaws.com/out/v1/2b08b86bcab54e9abdf685e816721e01/c60e5984a5a849c8828f58163dc8d020/50e4f858475a488a9b650313a8cc118b/index.m3u8',
-    'contentproducerId': 1002,
-    'publisherId': 1007,
-    'videoId': 5010,
-    'poster': 'https://d1pwmephoxhygn.cloudfront.net/VideoElephant/6cf22cd5-7529-4be7-80cb-b63f4c9a3fac/assets/Thumbnails/5d9d74f35c82b316cf46b3ba2e38a377.0000001.jpg',
-    'sequence': 0,
-    'title': 'By Blender Foundation',
+  'src': 'https://d8c1da9d356baba4ecb40074dc4758c5.egress.mediapackage-vod.us-east-1.amazonaws.com/out/v1/2b08b86bcab54e9abdf685e816721e01/c60e5984a5a849c8828f58163dc8d020/50e4f858475a488a9b650313a8cc118b/index.m3u8',
+  'contentproducerId': 1002,
+  'publisherId': 1007,
+  'videoId': 5010,
+  'poster': 'https://d1pwmephoxhygn.cloudfront.net/VideoElephant/6cf22cd5-7529-4be7-80cb-b63f4c9a3fac/assets/Thumbnails/5d9d74f35c82b316cf46b3ba2e38a377.0000001.jpg',
+  'sequence': 0,
+  'title': 'By Blender Foundation',
 }];
 
 const videoJson2 = [
-   {
+  {
     'src': 'https://d8c1da9d356baba4ecb40074dc4758c5.egress.mediapackage-vod.us-east-1.amazonaws.com/out/v1/1e0dd248d4ce433690da3946ef90cdf1/c60e5984a5a849c8828f58163dc8d020/50e4f858475a488a9b650313a8cc118b/index.m3u8',
-    'contentproducerId': 1002,
-    'publisherId': 1007,
-    'videoId': 1340,
+    'contentproducerId': 1005,
+    'publisherId': 1013,
+    'videoId': 20849,
     'poster': 'https://d1pwmephoxhygn.cloudfront.net/CampusLore/734164e033b711eca153b10917ac4f05/assets/d2fafd51-9a13-45b5-8cd3-3ef67de82e01/Thumbnails/734164e033b711eca153b10917ac4f05.0000001.jpg',
     'sequence': 0,
     'title': 'Calais Campbell: Be authentic and productive',
   },
   {
     'src': 'https://d8c1da9d356baba4ecb40074dc4758c5.egress.mediapackage-vod.us-east-1.amazonaws.com/out/v1/638a306434e84f82aef63c56ca43b531/c60e5984a5a849c8828f58163dc8d020/50e4f858475a488a9b650313a8cc118b/index.m3u8',
-    'contentproducerId': 1002,
-    'publisherId': 1007,
-    'videoId': 1305,
+    'contentproducerId': 1005,
+    'publisherId': 1013,
+    'videoId': 20851,
     'poster': 'https://d1pwmephoxhygn.cloudfront.net/CampusLore/2b42931033a011ec9bb6070f47828f9a/assets/48ae80a5-f4f1-4ce0-9087-1af36cc04bcf/Thumbnails/2b42931033a011ec9bb6070f47828f9a.0000001.jpg',
     'sequence': 1,
     'title': '2021 NFL Rookie Preview: Linebackers',
   },
   {
     'src': 'https://d8c1da9d356baba4ecb40074dc4758c5.egress.mediapackage-vod.us-east-1.amazonaws.com/out/v1/8f36ce58202c4156b9f127f373dc529e/c60e5984a5a849c8828f58163dc8d020/50e4f858475a488a9b650313a8cc118b/index.m3u8',
-    'contentproducerId': 1002,
-    'publisherId': 1007,
-    'videoId': 1296,
+    'contentproducerId': 1005,
+    'publisherId': 1013,
+    'videoId': 20846,
     'poster': 'https://d1pwmephoxhygn.cloudfront.net/CampusLore/dfa9ba80339c11ec9bb6070f47828f9a/assets/6e419954-2147-48b3-a032-79bd87640efa/Thumbnails/dfa9ba80339c11ec9bb6070f47828f9a.0000001.jpg',
     'sequence': 2,
     'title': '2021 NFL Rookie Preview: Wide Receivers',
@@ -105,9 +105,9 @@ function chanceBid(req) {
       console.log('20% probability of getting true', 'vpaidlinear', _chanceBidUrl);
       resolve();
     } else if (Math.random() < 0.3) {
-      _chanceBidUrl =  'https://09nfgyvbtl.execute-api.us-east-1.amazonaws.com/prod/ads?' + 'videoId=' + req.videoId + '&prdtenant=' + req.prdtenant + '&pubtenant=' + req.pubtenant +
+      _chanceBidUrl = 'https://09nfgyvbtl.execute-api.us-east-1.amazonaws.com/prod/ads?' + 'videoId=' + req.videoId + '&prdtenant=' + req.prdtenant + '&pubtenant=' + req.pubtenant +
           '&pubIp=' + req.pubIp + '&deviceType=' + req.deviceType + '&referrer=' + req.referrer + '&ua=' + req.ua + '&playlistId=' + req.playlistId;
-//           'https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/single_ad_samples&ciu_szs=300x250&impl=s&gdfp_req=1&env=vp&output=vast&unviewed_position_start=1&cust_params=deployment%3Ddevsite%26sample_ct%3Dredirectlinear&correlator=';
+      //           'https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/single_ad_samples&ciu_szs=300x250&impl=s&gdfp_req=1&env=vp&output=vast&unviewed_position_start=1&cust_params=deployment%3Ddevsite%26sample_ct%3Dredirectlinear&correlator=';
       console.log('30% probability of getting true', 'AWS', _chanceBidUrl);
     } else if (Math.round() < 0.4) {
       _chanceBidUrl = 'https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/ad_rule_samples&ciu_szs=300x250&ad_rule=1&impl=s&gdfp_req=1&env=vp&output=vmap&unviewed_position_start=1&cust_params=deployment%3Ddevsite%26sample_ar%3Dpremidpostoptimizedpodbumper&cmsid=496&vid=short_onecue&correlator=';
@@ -117,19 +117,17 @@ function chanceBid(req) {
       _chanceBidUrl = 'https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/ad_rule_samples&ciu_szs=300x250&ad_rule=1&impl=s&gdfp_req=1&env=vp&output=vmap&unviewed_position_start=1&cust_params=deployment%3Ddevsite%26sample_ar%3Dpremidpost&cmsid=496&vid=short_onecue&correlator=';
       console.log('50% probability of getting true', 'vmap1', _chanceBidUrl);
       resolve();
-    } else if((Math.random() < 0.6)){
-           _chanceBidUrl = 'https://publisherfenwickdemo.s3.amazonaws.com/ads/SkiingCampaign.xml';
+    } else if ((Math.random() < 0.6)) {
+      _chanceBidUrl = 'https://publisherfenwickdemo.s3.amazonaws.com/ads/SkiingCampaign.xml';
       console.log('60% probability of getting true', 'preroll skiingcampaign', _chanceBidUrl);
       resolve();
-      }
-      else if((Math.random() < 0.7)){
-           _chanceBidUrl = 
+    } else if ((Math.random() < 0.7)) {
+      _chanceBidUrl =
                'https://09nfgyvbtl.execute-api.us-east-1.amazonaws.com/prod/ads?' + 'videoId=' + req.videoId + '&prdtenant=' + req.prdtenant + '&pubtenant=' + req.pubtenant +
           '&pubIp=' + req.pubIp + '&deviceType=' + req.deviceType + '&referrer=' + req.referrer + '&ua=' + req.ua + '&playlistId=' + req.playlistId;
       console.log('70% probability of getting true', 'AWS', _chanceBidUrl);
       resolve();
-      }
-      else if (Math.random() < 0.8) {
+    } else if (Math.random() < 0.8) {
       _chanceBidUrl = 'https://secure.adnxs.com/ssptv?id=21641293' + '&referrer=' + req.referrer + '&vwidth=' + 920 + '&vheight=' + 780 + '&ua=' + req.ua;
       console.log('80% probability of getting true', 'preroll xandr', _chanceBidUrl);
       resolve();
@@ -150,18 +148,41 @@ app.get('/ssp', cors({credentials: true, origin: 'https://imasdk.googleapis.com'
   });
 });
 
-app.get('/ads',timeout(30000), cors({credentials: true, origin: 'https://imasdk.googleapis.com'}), function(req, res) {
+app.get('/ads', timeout(30000), cors({credentials: true, origin: 'https://imasdk.googleapis.com'}), function(req, res) {
   _chanceBidUrl = '';
   console.log('request received', _chanceBidUrl, req.query);
-  chanceBid(req.query).then(() => {
-    console.log('then', _chanceBidUrl);
-    makeXandrCall(_chanceBidUrl).then((response) => {
-        console.log(response);
-      res.type('application/xml');
-      res.send(response);
-    }).catch((error) => {
-      res.send(error);
-    });
+  // chanceBid(req.query).then(() => {
+  //   console.log('then', _chanceBidUrl);
+  //   makeXandrCall(_chanceBidUrl).then((response) => {
+  //     console.log(response);
+  //     res.type('application/xml');
+  //     res.send(response);
+  //   }).catch((error) => {
+  //     res.send(error);
+  //   });
+  // });
+  const _newUrl = 'https://09nfgyvbtl.execute-api.us-east-1.amazonaws.com/prod/ads?' +
+  'videoId=' +
+  req.videoId +
+  '&prdtenant=' +
+  req.prdtenant +
+  '&pubtenant=' +
+  req.pubtenant +
+  '&pubIp=' +
+  req.pubIp +
+  '&deviceType=' +
+  req.deviceType +
+  '&referrer=' +
+  req.referrer +
+  '&playlistId=' +
+  req.playlistId;
+
+  makeXandrCall(_newUrl).then((response) => {
+    console.log(response);
+    res.type('application/xml');
+    res.send(response);
+  }).catch((error) => {
+    res.send(error);
   });
 });
 
@@ -181,7 +202,7 @@ app.get('/getplaylistvideos', cors({
   'methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
 }), function(req, res) {
   console.log(req.query);
-  if (Number(req.query.playlistId) === 45678) {
+  if (Number(req.query.playlistId) === 1640167487) {
     res.json(videoJson2);
   } else if (Number(req.query.playlistId) === 45679) {
     res.json(videoJson3);
